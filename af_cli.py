c01283b81109d0773c70c24035fef625f6fcb75d
Kevin Thomas <kevin@mytechnotalent.com>
2020-09-09 07:47:15 -0400

    Update af_cli.py

--- af_cli.py
+++ af_cli.py
@@ -1,3 +1,23 @@
+#
+# AF
+# **
+#
+# Created by Kevin Thomas 09/09/20
+# Modified by Kevin Thomas 09/09/20
+# 
+# Copyright: (c) 2020, Kevin Thomas <kevin@mytechnotalent.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+# An open-source Automation Framework.
+# 
+# required modules
+# pip install ansible
+# pip install selenium
+#
+# Version 0.0.1 Alpha
+#
+
+
 import os
 import subprocess
 
