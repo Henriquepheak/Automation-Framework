57fc32445971cfece166c13872a0293823f94b74
Kevin Thomas <kevin@mytechnotalent.com>
2021-03-12 10:25:06 -0500

    Update

--- af_cli.py
+++ af_cli.py
@@ -3,7 +3,7 @@
 # ********************
 #
 # Created by Kevin Thomas 09/09/20
-# Modified by Kevin Thomas 09/09/20
+# Modified by Kevin Thomas 03/12/21
 # 
 # Copyright: (c) 2020, Kevin Thomas <kevin@mytechnotalent.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
