405bf4cea8a011e911b05d626b81b7fdb28b23b7
Kevin Thomas <kevin@mytechnotalent.com>
2021-03-13 12:17:20 -0500

    Update

--- library/twitter.py
+++ library/twitter.py
@@ -46,7 +46,7 @@ EXAMPLES = '''
 # Pass in a hashtag
 - name: Test twitter
   twitter:
-    hashtag: 'ansible'
+    hashtag: '#ansible'
     data_since: 2021-03-13
     number_of_tweets: 10
   register: test
@@ -69,75 +69,61 @@ import tweepy
 from secrets import *
 
 
-def __launch():
-    # allocate headless driver object
-    options = webdriver.ChromeOptions()
-    options.add_argument('window-size=1024x768')
-    options.add_argument('headless')
-    driver = webdriver.Chrome(options=options)
-    return driver
-
-
-def __query(driver, query, url, find_element_by, element, wait):
-    # query web and return response
-    element_result = None
-    driver.get('https://{}'.format(url))
-    if find_element_by == 'id':
-        # noinspection PyBroadException
+def __scrape(hashtag, date_since, number_of_tweets):
+    # perform data extraction and return response
+    # create DataFrame using pandas
+    db = pd.DataFrame(columns=['username', 'description', 'location', 'following',
+                               'followers', 'totaltweets', 'retweetcount', 'text', 'hashtags'])
+    # use .Cursor() to search through twitter for the required tweets
+    # the number of tweets can be restricted using .items(number of tweets)
+    tweets = tweepy.Cursor(api.search, q=hashtag, lang='en',
+                           since=date_since, tweet_mode='extended').items(number_of_tweets)
+    # .Cursor() returns an iterable object and each item in
+    # the iterator has various attributes that you can access to
+    # get information about each tweet
+    list_tweets = [tweet for tweet in tweets]
+    # we will iterate over each tweet in the list for extracting information about each tweet
+    for tweet in list_tweets:
+        username = tweet.user.screen_name
+        description = tweet.user.description
+        location = tweet.user.location
+        following = tweet.user.friends_count
+        followers = tweet.user.followers_count
+        totaltweets = tweet.user.statuses_count
+        retweetcount = tweet.retweet_count
+        hashtags = tweet.entities['hashtags']
+        # retweets can be distinguished by a retweeted_status attribute,
+        # in case it is an invalid reference, except block will be executed
         try:
-            WebDriverWait(driver, wait).until(
-                EC.presence_of_element_located((By.ID, element))
-            )
-        except:
-            driver.quit()
-        element_result = driver.find_element_by_id('{}'.format(element))
-    if find_element_by == 'name':
-        # noinspection PyBroadException
-        try:
-            WebDriverWait(driver, wait).until(
-                EC.visibility_of_element_located((By.NAME, element))
-            )
-        except:
-            driver.quit()
-        element_result = driver.find_element_by_name('{}'.format(element))
-    if find_element_by == 'xpath':
-        # noinspection PyBroadException
-        try:
-            WebDriverWait(driver, wait).until(
-                EC.presence_of_element_located((By.XPATH, element))
-            )
-        except:
-            driver.quit()
-        element_result = driver.find_element_by_xpath('{}'.format(element))
-    element_result.send_keys(query)
-    element_result.send_keys(Keys.RETURN)
-    element_result = driver.find_element_by_xpath('//*').text
-    driver.quit()
-    return element_result
+            text = tweet.retweeted_status.full_text
+        except AttributeError:
+            text = tweet.full_text
+        hashtext = list()
+        for j in range(0, len(hashtags)):
+            hashtext.append(hashtags[j]['text'])
+            # Append all the extracted information in the DataFrame
+        ith_tweet = [username, description, location, following,
+                     followers, totaltweets, retweetcount, text, hashtext]
+        db.loc[len(db)] = ith_tweet
+        return ith_tweet
 
 
 def run_module(module):
     # manipulate or modify the state as needed (this is going to be the
     # part where your module will do what it needs to do)
-    query = module.params['query']
-    url = module.params['url']
-    find_element_by = module.params['find_element_by']
-    element = module.params['element']
-    timeout = 60
+    hashtag = module.params['hashtag']
+    date_since = module.params['date_since']
+    number_of_tweets = module.params['number_of_tweets']
     element_result = None
-
-    # launch headless driver object and store in driver
-    driver = __launch()
 
     # noinspection PyBroadException
     try:
-        if query:
-            element_result = __query(driver, query, url, find_element_by, element, timeout)
+        if scrape:
+            element_result = __scrape(hashtag, date_since, number_of_tweets)
     except:
         # during the execution of the module, if there is an exception or a
         # conditional state that effectively causes a failure, run
         # AnsibleModule.fail_json() to pass in the message and the result
-        driver.quit()
         module.fail_json(msg='Can\'t locate element.')
 
     # seed the result dict in the object
@@ -161,10 +147,9 @@ def run_module(module):
 def main():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
-        query=dict(type='str', required=False),
-        url=dict(type='str', required=True),
-        find_element_by=dict(type='str', required=True),
-        element=dict(type='str', required=True)
+        hashtag=dict(type='str', required=True),
+        date_since=dict(type='str', required=True),
+        number_of_tweets=dict(type='int', required=True),
     )
 
     # the AnsibleModule object will be our abstraction working with Ansible
